fedea834a8ff930e22b7a746ad91a7a0670c43c8
Faris IOS <FarisAbuSaleem@sojoudahmads-Mac.local>
2014-06-10 05:58:45 +0300

    Initial Commit

---
+++ QuizApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QuizApp
+//
+//  Created by Faris IOS on 6/10/14.
+//  Copyright (c) 2014 Faris Abu Saleem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ QuizAppTests/QuizAppTests.m
@@ -0,0 +1,34 @@
+//
+//  QuizAppTests.m
+//  QuizAppTests
+//
+//  Created by Faris IOS on 6/10/14.
+//  Copyright (c) 2014 Faris Abu Saleem. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QuizAppTests : XCTestCase
+
+@end
+
+@implementation QuizAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
